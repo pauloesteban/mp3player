23b3b2bfc4d9cb349e5cf91a0dcd679f2e579b59
Orlando Brito Casanova <orlandobrito-rockqueen@hotmail.com>
2016-11-21 13:52:24 -0500

    Actualización de  panel de REPRODUCCIÓN

--- PepperMusic/src/peppermusic/jp_Reproduccion.java
+++ PepperMusic/src/peppermusic/jp_Reproduccion.java
@@ -8,8 +8,6 @@ package peppermusic;
 import com.sun.awt.AWTUtilities;
 import java.awt.Shape;
 import java.awt.geom.RoundRectangle2D;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 
 /**
@@ -23,6 +21,8 @@ public class jp_Reproduccion extends javax.swing.JPanel {
      */
     jp_Canciones canc;
     Clase_Progreso Barra;
+    int repetir=0;
+    
     public jp_Reproduccion(jp_Canciones can) {
         initComponents();
         canc =can;
@@ -31,7 +31,9 @@ public class jp_Reproduccion extends javax.swing.JPanel {
         START.setSelected(true);
         Barra = new Clase_Progreso(jp_Progreso,this);
         Barra.start();
-      
+      js_volumen.setVisible(false);
+        js_volumen.validate();
+        jb_repetir.setToolTipText("REPETICIÓN DESACTIVADA");
        
     }
 
@@ -50,8 +52,13 @@ public class jp_Reproduccion extends javax.swing.JPanel {
         jButton2 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         START = new peppermusic.CustomButton();
+        jButton4 = new javax.swing.JButton();
+        customButton1 = new peppermusic.CustomButton();
+        js_volumen = new javax.swing.JSlider();
+        jtb_aleatorio = new peppermusic.CustomButton();
+        jb_repetir = new javax.swing.JButton();
         jp_Progreso = new peppermusic.CustomPanel();
-        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
@@ -66,7 +73,7 @@ public class jp_Reproduccion extends javax.swing.JPanel {
             }
         });
         add(jtb_agrandar);
-        jtb_agrandar.setBounds(276, 130, 20, 20);
+        jtb_agrandar.setBounds(238, 315, 20, 20);
 
         karaoke.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/karaoke.png"))); // NOI18N
         karaoke.setBorder(null);
@@ -95,8 +102,13 @@ public class jp_Reproduccion extends javax.swing.JPanel {
         jButton1.setBorderPainted(false);
         jButton1.setContentAreaFilled(false);
         jButton1.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_avanzar2.png"))); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         add(jButton1);
-        jButton1.setBounds(225, 108, 30, 30);
+        jButton1.setBounds(220, 108, 30, 30);
 
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_retroceder.png"))); // NOI18N
         jButton2.setBorderPainted(false);
@@ -108,11 +120,11 @@ public class jp_Reproduccion extends javax.swing.JPanel {
             }
         });
         add(jButton2);
-        jButton2.setBounds(145, 108, 30, 30);
+        jButton2.setBounds(150, 108, 30, 30);
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/queen_ii.png"))); // NOI18N
         add(jLabel2);
-        jLabel2.setBounds(40, 60, 65, 65);
+        jLabel2.setBounds(50, 80, 70, 65);
 
         START.setBorder(null);
         START.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_start.png"))); // NOI18N
@@ -127,16 +139,89 @@ public class jp_Reproduccion extends javax.swing.JPanel {
             }
         });
         add(START);
-        START.setBounds(172, 52, 56, 56);
+        START.setBounds(172, 46, 56, 56);
+
+        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_fin.png"))); // NOI18N
+        jButton4.setBorder(null);
+        jButton4.setBorderPainted(false);
+        jButton4.setContentAreaFilled(false);
+        jButton4.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_fin2.png"))); // NOI18N
+        add(jButton4);
+        jButton4.setBounds(182, 116, 36, 36);
+
+        customButton1.setBorder(null);
+        customButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_volumen.png"))); // NOI18N
+        customButton1.setBorderPainted(false);
+        customButton1.setContentAreaFilled(false);
+        customButton1.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_volumen_sin.png"))); // NOI18N
+        customButton1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                customButton1MouseDragged(evt);
+            }
+            public void mouseMoved(java.awt.event.MouseEvent evt) {
+                customButton1MouseMoved(evt);
+            }
+        });
+        customButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                customButton1MouseExited(evt);
+            }
+        });
+        add(customButton1);
+        customButton1.setBounds(250, 120, 25, 25);
+
+        js_volumen.setOrientation(javax.swing.JSlider.VERTICAL);
+        js_volumen.setPaintLabels(true);
+        js_volumen.setToolTipText("");
+        js_volumen.setValue(80);
+        js_volumen.setDebugGraphicsOptions(javax.swing.DebugGraphics.NONE_OPTION);
+        js_volumen.setOpaque(false);
+        js_volumen.addContainerListener(new java.awt.event.ContainerAdapter() {
+            public void componentAdded(java.awt.event.ContainerEvent evt) {
+                js_volumenComponentAdded(evt);
+            }
+        });
+        js_volumen.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseMoved(java.awt.event.MouseEvent evt) {
+                js_volumenMouseMoved(evt);
+            }
+        });
+        js_volumen.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                js_volumenMouseExited(evt);
+            }
+        });
+        add(js_volumen);
+        js_volumen.setBounds(245, 36, 35, 90);
+
+        jtb_aleatorio.setBorder(null);
+        jtb_aleatorio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_aleatorio_off.png"))); // NOI18N
+        jtb_aleatorio.setBorderPainted(false);
+        jtb_aleatorio.setContentAreaFilled(false);
+        jtb_aleatorio.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_aleatorio_on.png"))); // NOI18N
+        add(jtb_aleatorio);
+        jtb_aleatorio.setBounds(120, 115, 20, 20);
+
+        jb_repetir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_rep_off.png"))); // NOI18N
+        jb_repetir.setBorder(null);
+        jb_repetir.setBorderPainted(false);
+        jb_repetir.setContentAreaFilled(false);
+        jb_repetir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jb_repetirActionPerformed(evt);
+            }
+        });
+        add(jb_repetir);
+        jb_repetir.setBounds(120, 88, 20, 20);
 
         jp_Progreso.setOpaque(false);
         jp_Progreso.setLayout(null);
         add(jp_Progreso);
-        jp_Progreso.setBounds(160, 40, 80, 80);
+        jp_Progreso.setBounds(160, 34, 80, 80);
 
-        jLabel4.setText("Bohemian Rhapsody");
-        add(jLabel4);
-        jLabel4.setBounds(28, 40, 130, 20);
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/repro_nombre.png"))); // NOI18N
+        add(jLabel5);
+        jLabel5.setBounds(30, 30, 120, 40);
 
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Barra Nie.png"))); // NOI18N
@@ -175,6 +260,7 @@ public class jp_Reproduccion extends javax.swing.JPanel {
     private void jtb_agrandarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtb_agrandarActionPerformed
         // TODO add your handling code here:
         if(jtb_agrandar.isSelected()){
+            jtb_agrandar.setLocation(238,150);
             
             canc.venta.setSize(300, 168);
             Shape forma = new RoundRectangle2D.Double(0,0,canc.venta.getBounds().width,canc.venta.getBounds().height,110,110);
@@ -187,6 +273,7 @@ public class jp_Reproduccion extends javax.swing.JPanel {
             jLabel1.revalidate();
         
         }else{
+           
             canc.venta.setSize(300, 336);
             Shape forma = new RoundRectangle2D.Double(0,0,canc.venta.getBounds().width,canc.venta.getBounds().height,125,125);
         AWTUtilities.setWindowShape(canc.venta, forma);
@@ -196,6 +283,7 @@ public class jp_Reproduccion extends javax.swing.JPanel {
             jLabel1.repaint();
             canc.jtb_lista.setEnabled(true);
             jLabel1.revalidate();
+             jtb_agrandar.setLocation(238,315);
         }
         
     }//GEN-LAST:event_jtb_agrandarActionPerformed
@@ -217,17 +305,89 @@ public class jp_Reproduccion extends javax.swing.JPanel {
 
     }//GEN-LAST:event_STARTActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void customButton1MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_customButton1MouseDragged
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_customButton1MouseDragged
+
+    private void customButton1MouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_customButton1MouseMoved
+        // TODO add your handling code here:
+         try{ Thread.sleep(175); } catch(InterruptedException e ) { System.out.println("Thread Interrupted"); }
+         js_volumen.setVisible(true);
+        js_volumen.validate();
+    }//GEN-LAST:event_customButton1MouseMoved
+
+    private void customButton1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_customButton1MouseExited
+        // TODO add your handling code here:
+         try{ Thread.sleep(250); } catch(InterruptedException e ) { System.out.println("Thread Interrupted"); }
+        js_volumen.setVisible(false);
+        js_volumen.validate();
+    }//GEN-LAST:event_customButton1MouseExited
+
+    private void js_volumenMouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_js_volumenMouseMoved
+        // TODO add your handling code here:
+         try{ Thread.sleep(175); } catch(InterruptedException e ) { System.out.println("Thread Interrupted"); }
+        js_volumen.setVisible(true);
+        js_volumen.validate();
+    }//GEN-LAST:event_js_volumenMouseMoved
+
+    private void js_volumenMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_js_volumenMouseExited
+        // TODO add your handling code here:
+         try{ Thread.sleep(250); } catch(InterruptedException e ) { System.out.println("Thread Interrupted"); }
+         js_volumen.setVisible(false);
+        js_volumen.validate();
+    }//GEN-LAST:event_js_volumenMouseExited
+
+    private void js_volumenComponentAdded(java.awt.event.ContainerEvent evt) {//GEN-FIRST:event_js_volumenComponentAdded
+        // TODO add your handling code here:
+       
+       
+    }//GEN-LAST:event_js_volumenComponentAdded
+
+    private void jb_repetirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_repetirActionPerformed
+        // TODO add your handling code here:
+        if(repetir==0){
+            jb_repetir.setIcon(new ImageIcon( getClass().getResource("/Recursos/repro_rep_1.png")));
+            jb_repetir.setToolTipText("REPETIR CANCIÓN");
+            repetir++;
+            return;
+        }
+        if(repetir==1){
+            jb_repetir.setIcon(new ImageIcon( getClass().getResource("/Recursos/repro_rep_todo.png")));
+            jb_repetir.setToolTipText("REPETIR TODAS LAS CANCIÓNES");
+            repetir++;
+            return;
+        }
+        if(repetir==2){
+            jb_repetir.setIcon(new ImageIcon( getClass().getResource("/Recursos/repro_rep_off.png")));
+            jb_repetir.setToolTipText("REPETICIÓN DESACTIVADA");
+            repetir=0;
+            
+        }
+        
+        
+    }//GEN-LAST:event_jb_repetirActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public peppermusic.CustomButton START;
+    private peppermusic.CustomButton customButton1;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JButton jb_repetir;
     private peppermusic.CustomPanel jp_Progreso;
+    private javax.swing.JSlider js_volumen;
     private javax.swing.JToggleButton jtb_agrandar;
+    private peppermusic.CustomButton jtb_aleatorio;
     private javax.swing.JToggleButton karaoke;
     // End of variables declaration//GEN-END:variables
 }
